4fd3eb5467eac22c8f3bc0acd65b0d0037a78e3a
Agustin-desktop <agustin2051@gmail.com>
2024-01-19 00:18:40 -0300

    ANALYTICID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Script from "next/script";
+
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 export default function Home() {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${googleAnalyticsId}');
+        `}
+      </Script>
       <Hero />
     </>
   );
