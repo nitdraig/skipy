691ca5bafccb84b2bf5c8bef9bd8a834d42b6e49
Agustin-desktop <agustin2051@gmail.com>
2024-01-19 20:21:11 -0300

    [Suggestions Button]

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,13 +9,23 @@ import Switcher from "./darkMode/Switch";
 
 export default function Nav() {
   return (
-    <Navbar className="z-50 " position="static">
+    <Navbar className="z-50" position="static">
       <NavbarBrand>
         <img className="h-36 w-48 mt-8 sm:ml-[-6em]" src="/logo.png" />
       </NavbarBrand>
       <NavbarContent justify="end">
         <NavbarItem className="mt-8">
           <Switcher />
+        </NavbarItem>
+        <NavbarItem className="mt-8 ">
+          <a
+            href="https://skipy.canny.io/sugerencias"
+            target="_blank"
+            className="px-3  py-1 shadow-lg shadow-[#387AF9]/50 bg-[#1A45DB] text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97]"
+          >
+            Sugerencias
+          </a>{" "}
+          &#128072;
         </NavbarItem>
       </NavbarContent>
     </Navbar>
