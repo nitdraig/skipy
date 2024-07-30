9541a112ebd1c17272b0c5422544033eaa342465
Agustin-desktop <agustin2051@gmail.com>
2024-07-30 19:10:33 -0300

    mod: get function api

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,17 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send(`
+     <title>Skipy API | Your Multipurpose Tool for Developers</title>
+     <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+        <h1>Welcome to Skipy API </h1>
+        <p>Spiky is a multi-tool application designed to make the lives of developers and IT professionals easier. Offers a variety of useful utilities for daily tasks, from generating shortened links to creating credit card information for testing. The application is designed to be centralized and in the future we plan to add more tools to expand its usefulness.<p/>
+        <p>This is the root of the API. For more information, see the <a href="https://skipy.top" style="color: #2b4c7e;">Documentation</a></p>
+        <a href="https://skipy.top" style="color: #2b4c7e;"> -> Skipy Web <- </a>
+     </div>
+  `);
+});
 app.use(
   cors({
     origin: "https://skipy.top",
